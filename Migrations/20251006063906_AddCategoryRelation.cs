c87e184b6075a359a919ce233ec55ebdae32e021
Devesh Rawal <devesh.facile@outlook.com>
2025-10-06 14:28:43 +0530

    Initial

---
+++ Migrations/20251006063906_AddCategoryRelation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CaregoryId",
+                table: "Items");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CaregoryId",
+                table: "Items",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CaregoryId",
+                value: null);
+        }
+    }
+}
---
+++ Models/SerialNumber.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyApp.Models
+{
+    public class SerialNumber
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        [ForeignKey("ItemId")]
+        public int ItemId { get; set; }
+
+
+       
+
+        public Item? Item { get; set; }
+    }
+}
